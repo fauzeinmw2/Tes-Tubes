d1fc6a4986499142ef29e93b70650b1544791b13
diasdigital <78405123+diasdigital@users.noreply.github.com>
2024-04-08 00:05:18 +0700

    Ubah hello situ

--- src/main/java/com/timsitu/testubes/TesTubes.java
+++ src/main/java/com/timsitu/testubes/TesTubes.java
@@ -11,6 +11,6 @@ package com.timsitu.testubes;
 public class TesTubes {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Situ");
     }
 }
